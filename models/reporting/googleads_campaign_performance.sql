d90dc0fd4c0c9c53847c402cb3f6cbb02a65c914
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-09 12:45:54 -0600

    Update googleads_campaign_performance.sql
    campaign_type_custom

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -8,6 +8,15 @@ campaign_name,
 campaign_id,
 campaign_status,
 campaign_type_default,
+CASE WHEN campaign_name IN 
+    ('[CALIFORNIA] HRT-Estrogen_|_ch:googlesearch_|_ob:Nonbrand_|_gl:impressions_|_tt:keywords_|_dv:all_|_ge:states_|_c1:_|_c2:_|_c3:_|_jump',
+    '[Superbolt] HRT Estrogen - NB Search - Florida',
+    '[Superbolt] HRT Estrogen - NB Search - New York',
+    '[Superbolt] HRT Estrogen - NB Search - Texas',
+    '[Superbolt] HRT Estrogen - NB Search - Online Estrogen - All States',
+    '[Superbolt] HRT Estrogen - NB Search - Remaining States') THEN 'NB Estrogen'
+    ELSE 'Other'
+END as campaign_type_custom,
 date,
 date_granularity,
 spend,
