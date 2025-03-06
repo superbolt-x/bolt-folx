f4e872be1664daa38bc4991b84ee67c952c3d913
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-06 14:14:40 -0500

    Create facebook_campaign_performance.sql

---
+++ models/reporting/facebook_campaign_performance.sql
@@ -0,0 +1,15 @@
+{{ config (
+    alias = target.database + '_facebook_campaign_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_effective_status,
+campaign_type_default,
+date,
+date_granularity,
+spend,
+impressions,
+link_clicks
+FROM {{ ref('facebook_performance_by_campaign') }}
