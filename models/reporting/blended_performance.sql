9170ebfda400f7e8d932d30b94420798a13fa771
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-17 15:32:33 -0400

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -1,5 +1,5 @@
 {{ config (
-    alias = target.database + '_folx_blended_performance'
+    alias = target.database + '_blended_performance'
 )}}
 
 -- Facebook data
@@ -68,7 +68,7 @@ UNION ALL
 
 -- Trials/Membership data
 (SELECT 
-    'Organic' as channel,
+    'Memberships' as channel,
     NULL as campaign_name,
     date,
     'day' as date_granularity,
@@ -78,38 +78,4 @@ UNION ALL
     COALESCE(SUM(trials), 0) as trials,
     COALESCE(SUM(memberships), 0) as memberships
 FROM {{ source('gsheet_raw', 'memberships') }}
-GROUP BY channel, campaign_name, date, date_granularity)
-
-UNION ALL
-
--- Trials/Membership data with attributed spend from Meta
-(SELECT 
-    'Facebook' as channel,
-    NULL as campaign_name,
-    date,
-    'day' as date_granularity,
-    COALESCE(SUM(meta_spend), 0) as spend,
-    0 as impressions,
-    0 as clicks,
-    COALESCE(SUM(trials), 0) as trials,
-    COALESCE(SUM(memberships), 0) as memberships
-FROM {{ source('gsheet_raw', 'memberships') }}
-WHERE meta_spend IS NOT NULL AND meta_spend > 0
-GROUP BY channel, campaign_name, date, date_granularity)
-
-UNION ALL
-
--- Trials/Membership data with attributed spend from Google Adwords
-(SELECT 
-    'Google' as channel,
-    NULL as campaign_name,
-    date,
-    'day' as date_granularity,
-    COALESCE(SUM(adw_spend), 0) as spend,
-    0 as impressions,
-    0 as clicks,
-    COALESCE(SUM(trials), 0) as trials,
-    COALESCE(SUM(memberships), 0) as memberships
-FROM {{ source('gsheet_raw', 'memberships') }}
-WHERE adw_spend IS NOT NULL AND adw_spend > 0
 GROUP BY channel, campaign_name, date, date_granularity)
