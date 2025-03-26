aa466d546de8a66e0d82a20425f55d263587e4e2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-26 11:34:06 -0600

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -5,7 +5,7 @@
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
 WITH initial_memb_data as
-    (SELECT *, {{ get_date_parts('date') }}
+    (SELECT *, date::date as date, {{ get_date_parts('date') }}
     FROM {{ source('gsheet_raw', 'memberships') }}
     )
     
