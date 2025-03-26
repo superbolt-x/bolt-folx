4068860077d22fc7418d9adea7ce4c172badb53a
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-26 11:04:10 -0600

    Update blended_performance.sql
    memb_data

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -4,8 +4,13 @@
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
+WITH initial_memb_data as
+    (SELECT *, {{ get_date_parts('date') }}
+    FROM {{ source('gsheet_raw', 'memberships') }}
+    )
+    
 -- Facebook data
-(SELECT 
+SELECT 
     'Facebook' as channel,
     campaign_name,
     date,
@@ -16,12 +21,12 @@
     0 as trials,
     0 as memberships
 FROM {{ source('reporting', 'facebook_campaign_performance') }}
-GROUP BY channel, campaign_name, date, date_granularity)
+GROUP BY channel, campaign_name, date, date_granularity
 
 UNION ALL
 
 -- Google Adwords data
-(SELECT 
+SELECT 
     'Google' as channel,
     campaign_name,
     date,
@@ -32,12 +37,12 @@ UNION ALL
     0 as trials,
     0 as memberships
 FROM {{ source('reporting', 'googleads_campaign_performance') }}
-GROUP BY channel, campaign_name, date, date_granularity)
+GROUP BY channel, campaign_name, date, date_granularity
 
 UNION ALL
 
 -- TikTok data
-(SELECT 
+SELECT 
     'TikTok' as channel,
     campaign_name,
     date,
@@ -48,12 +53,12 @@ UNION ALL
     0 as trials,
     0 as memberships
 FROM {{ source('reporting', 'tiktok_ad_performance') }}
-GROUP BY channel, campaign_name, date, date_granularity)
+GROUP BY channel, campaign_name, date, date_granularity
 
 UNION ALL
 
 -- Reddit data
-(SELECT 
+SELECT 
     'Reddit' as channel,
     campaign_name,
     date,
@@ -64,13 +69,13 @@ UNION ALL
     0 as trials,
     0 as memberships
 FROM {{ source('reporting', 'reddit_performance_by_ad') }}
-GROUP BY channel, campaign_name, date, date_granularity)
+GROUP BY channel, campaign_name, date, date_granularity
 
 UNION ALL
 
 -- Trials/Membership data with all date granularities
 {% for date_granularity in date_granularity_list %}
-(SELECT 
+SELECT 
     'Memberships' as channel,
     NULL as campaign_name,
     date_trunc('{{ date_granularity }}', date)::date AS date,
@@ -80,7 +85,7 @@ UNION ALL
     0 as clicks,
     COALESCE(SUM(trials), 0) as trials,
     COALESCE(SUM(memberships), 0) as memberships
-FROM {{ source('gsheet_raw', 'memberships') }}
-GROUP BY 1,2,3,4)
+FROM initial_memb_data
+GROUP BY 1,2,3,4
 {% if not loop.last %}UNION ALL{% endif %}
 {% endfor %}
