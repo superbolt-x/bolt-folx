a01492725d0e7fc0db9ca12f471d80c29a0ad04a
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-27 15:42:32 -0600

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -4,83 +4,38 @@
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
--- Facebook data
-(SELECT 
-    'Facebook' as channel,
-    campaign_name,
-    date,
-    date_granularity,
-    COALESCE(SUM(spend), 0) as spend,
-    COALESCE(SUM(impressions), 0) as impressions,
-    COALESCE(SUM(link_clicks), 0) as clicks,
-    0 as trials,
-    0 as memberships
-FROM {{ source('reporting', 'facebook_campaign_performance') }}
-GROUP BY channel, campaign_name, date, date_granularity)
+WITH initial_memb_data as
+    (SELECT *, date::date as date_adj, {{ get_date_parts('date_adj') }}
+    FROM {{ source('gsheet_raw', 'memberships') }}
+    )
 
-UNION ALL
-
--- Google Adwords data
-(SELECT 
-    'Google' as channel,
-    campaign_name,
-    date,
-    date_granularity,
-    COALESCE(SUM(spend), 0) as spend,
-    COALESCE(SUM(impressions), 0) as impressions,
-    COALESCE(SUM(clicks), 0) as clicks,
-    0 as trials,
-    0 as memberships
-FROM {{ source('reporting', 'googleads_campaign_performance') }}
-GROUP BY channel, campaign_name, date, date_granularity)
-
-UNION ALL
-
--- TikTok data
-(SELECT 
-    'TikTok' as channel,
-    campaign_name,
-    date,
-    date_granularity,
-    COALESCE(SUM(spend), 0) as spend,
-    COALESCE(SUM(impressions), 0) as impressions,
-    COALESCE(SUM(clicks), 0) as clicks,
-    0 as trials,
-    0 as memberships
-FROM {{ source('reporting', 'tiktok_ad_performance') }}
-GROUP BY channel, campaign_name, date, date_granularity)
-
-UNION ALL
-
--- Reddit data
-(SELECT 
-    'Reddit' as channel,
-    campaign_name,
-    date,
-    date_granularity,
-    COALESCE(SUM(spend), 0) as spend,
-    COALESCE(SUM(impressions), 0) as impressions,
-    COALESCE(SUM(clicks), 0) as clicks,
-    0 as trials,
-    0 as memberships
-FROM {{ source('reporting', 'reddit_performance_by_campaign') }}
-GROUP BY channel, campaign_name, date, date_granularity)
-
-UNION ALL
-
--- Trials/Membership data with all date granularities
-{% for date_granularity in date_granularity_list %}
-(SELECT 
-    'Memberships' as channel,
-    NULL as campaign_name,
-    date_trunc('{{ date_granularity }}', date)::date AS date,
-    '{{ date_granularity }}' as date_granularity,
-    0 as spend,
-    0 as impressions,
-    0 as clicks,
-    COALESCE(SUM(trials), 0) as trials,
-    COALESCE(SUM(memberships), 0) as memberships
-FROM {{ source('gsheet_raw', 'memberships') }}
-GROUP BY 1,2,3,4)
-{% if not loop.last %}UNION ALL{% endif %}
-{% endfor %}
+SELECT channel, campaign_name, date, date_granularity, COALESCE(SUM(spend), 0) as spend, COALESCE(SUM(impressions), 0) as impressions, COALESCE(SUM(clicks), 0) as clicks,
+    COALESCE(SUM(trials), 0) as trials, COALESCE(SUM(memberships), 0) as memberships
+FROM
+        ({% for date_granularity in date_granularity_list %}
+        SELECT 
+            'Facebook' as channel, campaign_name::varchar, date::date, date_granularity::varchar, 
+                spend, impressions, link_clicks as clicks, 0 as trials, 0 as memberships
+        FROM {{ source('reporting', 'facebook_campaign_performance') }}
+        UNION ALL
+        SELECT 
+            'Google' as channel, campaign_name::varchar, date::date, date_granularity::varchar, 
+                spend, impressions, clicks, 0 as trials, 0 as memberships
+        FROM {{ source('reporting', 'googleads_campaign_performance') }}        
+        UNION ALL
+        SELECT 
+            'TikTok' as channel, campaign_name::varchar, date::date, date_granularity::varchar, 
+                spend, impressions, clicks, 0 as trials, 0 as memberships
+        FROM {{ source('reporting', 'tiktok_ad_performance') }}
+        UNION ALL
+        SELECT 
+            'Reddit' as channel, campaign_name::varchar, date::date, date_granularity::varchar, 
+                spend, impressions, clicks, 0 as trials, 0 as memberships
+        FROM {{ source('reporting', 'reddit_performance_by_ad') }}
+        UNION ALL
+        SELECT 'Memberships' as channel, NULL::varchar as campaign_name, '{{date_granularity}}'::varchar as date_granularity, {{date_granularity}}::date as date,
+            0 as spend, 0 as impressions, 0 as clicks, trials, memberships
+        FROM initial_memb_data
+        {% if not loop.last %}UNION ALL{% endif %}
+        {% endfor %})
+GROUP BY channel, campaign_name, date, date_granularity
