b60ef632fcb200aa4208f7beed7e1c1cce969edd
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-28 14:02:17 -0400

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -13,27 +13,7 @@ SELECT channel, campaign_name, date, date_granularity, COALESCE(SUM(spend), 0) a
     COALESCE(SUM(trials), 0) as trials, COALESCE(SUM(memberships), 0) as memberships
 FROM
         ({% for date_granularity in date_granularity_list %}
-        SELECT 
-            'Facebook' as channel, campaign_name, date::date as date, date_granularity, 
-                spend, impressions, link_clicks as clicks, 0 as trials, 0 as memberships
-        FROM {{ source('reporting', 'facebook_campaign_performance') }}
-        UNION ALL
-        SELECT 
-            'Google' as channel, campaign_name, date::date as date, date_granularity, 
-                spend, impressions, clicks, 0 as trials, 0 as memberships
-        FROM {{ source('reporting', 'googleads_campaign_performance') }}        
-        UNION ALL
-        SELECT 
-            'TikTok' as channel, campaign_name, date::date as date, date_granularity, 
-                spend, impressions, clicks, 0 as trials, 0 as memberships
-        FROM {{ source('reporting', 'tiktok_ad_performance') }}
-        UNION ALL
-        SELECT 
-            'Reddit' as channel, campaign_name, date::date as date, date_granularity, 
-                spend, impressions, clicks, 0 as trials, 0 as memberships
-        FROM {{ source('reporting', 'reddit_performance_by_ad') }}
-        UNION ALL
-        SELECT 'Memberships' as channel, NULL::varchar as campaign_name, '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
+        SELECT 'Memberships' as channel, NULL::varchar as campaign_name, {{date_granularity}} as date, '{{date_granularity}}' as date_granularity, 
             0 as spend, 0 as impressions, 0 as clicks, trials, memberships
         FROM initial_memb_data
         {% if not loop.last %}UNION ALL{% endif %}
