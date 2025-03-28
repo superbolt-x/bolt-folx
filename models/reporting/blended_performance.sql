e2b6c52361e70cf39fa4b2dd9cc5c217ab5aabd2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-28 13:49:05 -0400

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -4,6 +4,11 @@
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
+WITH initial_memb_data as
+    (SELECT *, {{ get_date_parts('date') }}
+    FROM {{ source('gsheet_raw', 'memberships') }}
+    )
+
 SELECT channel, campaign_name, date, date_granularity, COALESCE(SUM(spend), 0) as spend, COALESCE(SUM(impressions), 0) as impressions, COALESCE(SUM(clicks), 0) as clicks,
     COALESCE(SUM(trials), 0) as trials, COALESCE(SUM(memberships), 0) as memberships
 FROM
@@ -27,6 +32,10 @@ FROM
             'Reddit' as channel, campaign_name, date::date as date, date_granularity, 
                 spend, impressions, clicks, 0 as trials, 0 as memberships
         FROM {{ source('reporting', 'reddit_performance_by_ad') }}
+        UNION ALL
+        SELECT 'Memberships' as channel, NULL::varchar as campaign_name, '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
+            0 as spend, 0 as impressions, 0 as clicks, trials::varchar a trials, memberships::varchar as memberships
+        FROM initial_memb_data
         {% if not loop.last %}UNION ALL{% endif %}
         {% endfor %})
 GROUP BY channel, campaign_name, date, date_granularity
