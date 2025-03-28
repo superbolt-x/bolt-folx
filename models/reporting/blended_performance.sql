cc571dbb7ce88d2b3fe19e46a96487783aeda751
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-28 13:28:58 -0400

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -4,10 +4,10 @@
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 
-WITH initial_memb_data as
-    (SELECT *, {{ get_date_parts('date') }}
-    FROM {{ source('gsheet_raw', 'memberships') }}
-    )
+--WITH initial_memb_data as
+--    (SELECT *, {{ get_date_parts('date') }}
+--    FROM {{ source('gsheet_raw', 'memberships') }}
+--    )
 
 SELECT channel, campaign_name, date, date_granularity, COALESCE(SUM(spend), 0) as spend, COALESCE(SUM(impressions), 0) as impressions, COALESCE(SUM(clicks), 0) as clicks,
     COALESCE(SUM(trials), 0) as trials, COALESCE(SUM(memberships), 0) as memberships
@@ -32,10 +32,10 @@ FROM
             'Reddit' as channel, campaign_name, date::date as date, date_granularity, 
                 spend, impressions, clicks, 0 as trials, 0 as memberships
         FROM {{ source('reporting', 'reddit_performance_by_ad') }}
-        UNION ALL
-        SELECT 'Memberships' as channel, NULL as campaign_name, '{{date_granularity}}' as date_granularity, {{date_granularity}}::date as date,
-            0 as spend, 0 as impressions, 0 as clicks, trials, memberships
-        FROM initial_memb_data
+--        UNION ALL
+--        SELECT 'Memberships' as channel, NULL as campaign_name, '{{date_granularity}}' as date_granularity, {{date_granularity}}::date as date,
+--            0 as spend, 0 as impressions, 0 as clicks, trials, memberships
+--        FROM {{ source('gsheet_raw', 'memberships') }}
         {% if not loop.last %}UNION ALL{% endif %}
         {% endfor %})
 GROUP BY channel, campaign_name, date, date_granularity
