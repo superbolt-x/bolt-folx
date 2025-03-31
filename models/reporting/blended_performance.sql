1bd12c211f668a2841ca8399a5fd09e714af2fc8
Vincent Gefflaut <vincent.gefflaut@superbolt.agency>
2025-03-31 15:08:32 -0400

    fix

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -71,7 +71,7 @@ FROM
             ELSE fp.{{date_granularity}}
         END as date, 
         '{{date_granularity}}' as date_granularity,
-        get_state_code('region') as region,
+        {{ get_state_code('region') }} as region,
         spend, 
         impressions, 
         inline_link_clicks as clicks, 
